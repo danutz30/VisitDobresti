0522033aab57a10a5831239102c86596234108db
Danut Anca <danyanca2002@gmail.com>
2018-03-31 14:50:17 +0300

    visit dobresti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AcasaComponent } from './components/acasa/acasa.component';
+import { IstoricComponent } from './components/istoric/istoric.component';
+import { GalerieComponent } from './components/galerie/galerie.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BlogComponent } from './components/blog/blog.component';
 
-
+const appRoutes: Routes = [
+  { path: 'acasa', component: AcasaComponent },
+  { path: '',   redirectTo: '/acasa', pathMatch: 'full' },
+  { path: 'istoric', component: IstoricComponent },
+  { path: 'galerie', component: GalerieComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcasaComponent,
+    IstoricComponent,
+    GalerieComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/acasa/acasa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acasa',
+  templateUrl: './acasa.component.html',
+  styleUrls: ['./acasa.component.scss']
+})
+export class AcasaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
