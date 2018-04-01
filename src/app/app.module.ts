a6fd7fa9895d1661bd1a7d8b36f32739197650b3
Danut Anca <danyanca2002@gmail.com>
2018-04-01 15:00:06 +0300

    firebase changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from './../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,6 +16,11 @@ import { GalerieComponent } from './components/galerie/galerie.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { EmployeeComponent } from './components/employees/employee/employee.component';
+import { EmployeeListComponent } from './components/employees/employee-list/employee-list.component';
+import { FormsModule} from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 const appRoutes: Routes = [
   { path: 'acasa', component: AcasaComponent },
@@ -30,15 +39,23 @@ const appRoutes: Routes = [
     GalerieComponent,
     ContactComponent,
     PageNotFoundComponent,
-    BlogComponent
+    BlogComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyCJkOhsNku9p2D2FFvZRq89FNx5Nt-UNq4',
+    authDomain: 'visitdobresti.firebaseapp.com',
+    databaseURL: 'https://visitdobresti.firebaseio.com',
+    projectId: 'visitdobresti',
+    storageBucket: 'visitdobresti.appspot.com',
+    messagingSenderId: '653641321805'
 };
